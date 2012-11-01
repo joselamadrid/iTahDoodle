a81890b56f8ffa0e3b9fa8760eb6758eefe9ea8a
Joselamadrid <sysadminktp@mail.ru>
2012-11-01 09:11:02 +0200

    Initial commit

---
+++ iTahDoodle/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  iTahDoodle
+//
+//  Created by Josemac on 01.11.12.
+//  Copyright (c) 2012 Jose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NSString* docPath();
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+{
+    UITableView *taskTable;
+    UITextField *taskField;
+    UIButton *insertButton;    
+    NSMutableArray *tasks;
+}
+
+- (void) addTask:(id)sender;
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iTahDoodle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iTahDoodle
+//
+//  Created by Josemac on 01.11.12.
+//  Copyright (c) 2012 Jose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
